fe81733001aea273bb6ed14e74abf0810ecec610
Vasu Rajan <vasurajan101@gmail.com>
2021-09-25 04:27:01 +0530

    added navbar links, cart and logout button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
+import {Home, About, Cart, Checkout, Error, SingleProduct, Private, Products } from './pages'
 
 function App() {
-  return <h4>At Ease Store</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/checkout">
+          <Checkout />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+        <Route exact path="/products">
+          <Products />
+        </Route>
+        <Route exact path="/products/:id">
+          <SingleProduct />
+        </Route>
+        <Route exact path="*">
+          <Error />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Home from './HomePage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+import Products from './ProductsPage';
+import Error from './ErrorPage';
+import SingleProduct from './SingleProductPage';
+import Private from './PrivateRoute';
+export {
+    Home,
+    About,
+    Cart,
+    Checkout,
+    Products,
+    Error,
+    SingleProduct,
+    Private
+}
