14eb4a29d256960a8c57a6638a51c6318cf8b60f
Vasu Rajan <vasurajan101@gmail.com>
2021-09-26 02:21:50 +0530

    added code for fetching data of single pproduct and showing main image with other images on it

--- src/components/ProductImages.js
+++ src/components/ProductImages.js
@@ -1,8 +1,25 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const ProductImages = () => {
-  return <h4>product images</h4>
+const ProductImages = ({images = [{url: ""}]}) => {
+  const [main, setMain] = useState(images[0])
+  return (
+    <Wrapper>
+      <img src={main.url} alt="main product" className='main' />
+      <div className="gallery">
+      {images.map((image, index) => {
+        return (
+          <img 
+          key={index}
+          src={image.url} 
+          alt={image.filename} 
+          className={`${image.url === main.url ? "active" : null}`}
+          onClick={() => setMain(images[index])} />
+        )
+      })}
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,8 +15,83 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
+  const {id} = useParams();
+  const history = useHistory()
+  const {single_product_loading, single_product_error, single_product, fetchSingleProduct} = useProductsContext()
+ 
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`)
+  }, [id]
+  )
+
+  console.log(single_product)
+  
+  useEffect(() => {
+    if(single_product_error){
+      setTimeout(() => {
+        history.push('/')
+      }, 3000)
+    }
+  }, [single_product_error])
+
+  if(single_product_loading){
+    return (
+     <Loading />
+    )  
+    }
+    if(single_product_error){
+      return (
+      <Error />
+      )
+    }
+
+    const {
+      name,
+      price,
+      description,
+      stock,
+      stars,
+      reviews,
+      id: sku,
+      company,
+      images,
+    } = single_product
+    return (
+      <Wrapper>
+        <PageHero title={name} product />
+        <div className='section section-center page'>
+          <Link to='/products' className='btn'>
+            back to products
+          </Link>
+          <div className=' product-center'>
+            <ProductImages images={images} />
+            <section className='content'>
+              <h2>{name}</h2>
+              <Stars stars={stars} reviews={reviews} />
+              <h5 className='price'> {formatPrice(price)}</h5>
+              <p className='desc'> {description}</p>
+              <p className='info'>
+                <span>Available : </span>
+                {stock > 0 ? 'In stock' : 'out of stock'}
+              </p>
+              <p className='info'>
+                <span>SKU : </span>
+                {sku}
+              </p>
+              <p className='info'>
+                <span>Brand : </span>
+                {company}
+              </p>
+              <hr />
+              {stock > 0 && <AddToCart product={single_product} />}
+            </section>
+          </div>
+        </div>
+      </Wrapper>
+    )
 }
+  
+
 
 const Wrapper = styled.main`
   .product-center {
